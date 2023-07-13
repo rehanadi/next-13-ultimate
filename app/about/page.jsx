c5a6c5d8d304baa617e8907f5cb2a67b42b7f6f3
Rehan <rehanadipurwana@gmail.com>
2023-07-13 13:12:50 +0700

    First commit

---
+++ app/about/page.jsx
@@ -0,0 +1,9 @@
+import styles from './page.module.css'
+
+const AboutPage = () => {
+  return (
+    <div className={styles.title}>About</div>
+  )
+}
+
+export default AboutPage
---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <section className='py-24'>
+      <div className='container'>
+        <h1 className={styles.title}>
+          NextJS 13 Starter Kit
+        </h1>
+      </div>
+    </section>
+  )
+}
